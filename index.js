43aded16cff4c0d4bea00043953e18fcdddfde6b
melanycpm1 <melanycatalanpabon@gmail.com>
2025-10-06 23:53:22 -0300

    feat: agregar link font awesone dentro de head

--- index.js
+++ index.js
@@ -7,6 +7,34 @@ async function main() {
     } catch (error) {
         console.log(`Error al obtener los datos:${error}`)
     }
+    
+    if(data){
+        try {
+            //querySelector
+            let nav= document.querySelector(".nav");
+            //createElement
+            let div=document.createElement("div");
+            let ul=document.createElement("ul");
+            //appendChild
+            nav.appendChild(div);
+            div.appendChild(ul);
+
+
+            //obtencion de keys
+            let arrayKey= Object.keys(data);
+
+
+            arrayKey.forEach(key => {
+                let li=document.createElement("li");
+                li.innerHTML=`<a href="#${key}">${key}</a>`;
+                ul.appendChild(li);
+            });
+
+
+        } catch (error) {
+            console.log(`Error al procesar los datos o el DOM: ${error}`);
+        }
+    }
 
 }
 

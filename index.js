25a23360805bd032150d6244d9b1551d98d9687c
melanycpm1 <melanycatalanpabon@gmail.com>
2025-10-07 01:43:47 -0300

    creacion de clase para los personajes

--- index.js
+++ index.js
@@ -1,3 +1,66 @@
+class Personaje {
+    constructor(nombre, rol, caracteristica, img, frases = []) {
+        this.nombre = nombre;
+        this.rol = rol;
+        this.caracteristica = caracteristica;
+        this.img = img;
+        this.frases = frases;
+    }
+    //setter y getter
+    getNombre() {
+        return this.nombre;
+    }
+
+    getRol() {
+        return this.rol;
+    }
+
+    getCaracteristica() {
+        return this.caracteristica;
+    }
+
+    getImg() {
+        return this.img;
+    }
+
+    getFrases() {
+        return this.frases;
+    }
+
+    setNombre(nombre) {
+        this.nombre = nombre;
+    }
+
+    setRol(rol) {
+        this.rol = rol;
+    }
+
+    setCaracteristica(caracteristica) {
+        this.caracteristica = caracteristica;
+    }
+
+    setImg(img) {
+        this.img = img;
+    }
+
+    setFrases(frases) {
+        this.frases = frases;
+    }
+    //metodos
+    agregarFrase(frase) {
+        this.frases.push(frase);
+    }
+
+    mostrarInfo() {
+        return `
+            <h3>${this.nombre}</h3>
+            <p><strong>Rol:</strong> ${this.rol}</p>
+            <p><strong>Característica:</strong> ${this.caracteristica}</p>
+            <p><strong>Frases:</strong> ${this.frases.join(", ")}</p>
+        `;
+    }
+}
+
 async function main() {
     let data;
     try {

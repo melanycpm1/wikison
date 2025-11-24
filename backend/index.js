158a68fe8975cfca33816114358478c1026c5252
melanycpm1 <melanycatalanpabon@gmail.com>
2025-11-24 18:37:31 -0300

    feat:  inicializando firebase

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,23 @@ const express = require('express')
 const app = express()
 const port = 3000
 const ROOT_FOLDER = 'dist'
-app.use(cors())
+require("dotenv").config();
+
 app.use(express.static(path.join(__dirname, ROOT_FOLDER)))
+app.use(cors())
+
+const admin = require("firebase-admin");
+const serviceAccountPath = process.env.SERVICE_ACCOUNT_PATH;
+
+const serviceAccount = require("./" + serviceAccountPath);
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: process.env.FIRESTORE_DB
+});
+
+const { getFirestore } = require("firebase-admin/firestore");
+const db = getFirestore();
 
 
 app.get('/', (req, res) => {
